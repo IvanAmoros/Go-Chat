e08bf75bd7eb2cacc9a8120d46bac6434e3f4b7c
iamoros <ivan13ap@gmail.com>
2024-10-15 12:26:08 +0200

    port print

--- main.go
+++ main.go
@@ -102,15 +102,16 @@ func main() {
     // Get the port from the environment (required by Heroku)
     port := os.Getenv("PORT")
     if port == "" {
-        port = "8080" // Default to 8080 if not running on Heroku
+        port = "8080" // Default to 8080 for local testing
         fmt.Println("PORT environment variable not set, using default port 8080")
     }
+
+    fmt.Printf("Server started on port %s\n", port) // Corrected log message
 
     http.HandleFunc("/ws", handleWebSocket)
 
     // Run the message broadcasting handler
     go handleMessages()
 
-    fmt.Println("Server started at :8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Fatal(http.ListenAndServe(":"+port, nil)) // Correctly bind to the port
 }
